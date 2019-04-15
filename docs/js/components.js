f5fe8a6010cb23f3fd00a09702e12bfc207e21d9
Kristy Barash <kristy@chapterthree.com>
2019-04-14 23:08:11 -0400

    desktop arrow adjustments

--- docs/js/components.js
+++ docs/js/components.js
@@ -1,4 +1,10 @@
+
 (function ($) {
+
+
+
+})(jQuery);
+;(function ($) {
 
 // Open Card Reveal Click
 $('.c-card--reveal').hover(function(){
@@ -7,4 +13,4 @@ $('.c-card--reveal').hover(function(){
 
 })(jQuery);
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNhcmQvY2FyZC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiY29tcG9uZW50cy5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbiAoJCkge1xuXG4vLyBPcGVuIENhcmQgUmV2ZWFsIENsaWNrXG4kKCcuYy1jYXJkLS1yZXZlYWwnKS5ob3ZlcihmdW5jdGlvbigpe1xuICAkKHRoaXMpLmZpbmQoJy5jLWNhcmRfX292ZXJsYXknKS50b2dnbGVDbGFzcygnaG92ZXInKTtcbn0pO1xuXG59KShqUXVlcnkpO1xuIl19
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIjAxLW5hdmlnYXRpb24vMDEtbWFpbi9tYWluLmpzIiwiY2FyZC9jYXJkLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLENDTkE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImNvbXBvbmVudHMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcbihmdW5jdGlvbiAoJCkge1xuXG5cblxufSkoalF1ZXJ5KTtcbiIsIihmdW5jdGlvbiAoJCkge1xuXG4vLyBPcGVuIENhcmQgUmV2ZWFsIENsaWNrXG4kKCcuYy1jYXJkLS1yZXZlYWwnKS5ob3ZlcihmdW5jdGlvbigpe1xuICAkKHRoaXMpLmZpbmQoJy5jLWNhcmRfX292ZXJsYXknKS50b2dnbGVDbGFzcygnaG92ZXInKTtcbn0pO1xuXG59KShqUXVlcnkpO1xuIl19
