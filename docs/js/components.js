5b6c55b8fd759d9b39596598108b37b577784a8f
Anne Bonham <anne.bonham@kanopistudios.com>
2019-04-30 09:53:11 -0700

    tw22874953 - more search theming

--- docs/js/components.js
+++ docs/js/components.js
@@ -45,6 +45,30 @@ $('.c-card--reveal').hover(function(){
 });
 
 })(jQuery);
+;/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($) {
+
+  /**
+   * Opens search filters modal.
+   */
+    // Open search filters.
+    $('#filter-button').click(function() {
+      $('#filters-modal').toggleClass('closed');
+    });
+    // Modal close button.
+    $('#filters-modal .close-button').click(function() {
+      $('#filters-modal').toggleClass('closed');
+    });
+
+
+})(jQuery);
+
+
+
 ;
 (function ($) {
 
