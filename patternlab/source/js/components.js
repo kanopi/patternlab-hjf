8513c33aa8beaca5cb1fec77a65929b11cae016c
Anne Bonham <anne.bonham@kanopistudios.com>
2019-05-06 16:30:52 -0700

    tw23841631-search-revisions - search submit button changes in progress

--- patternlab/source/js/components.js
+++ patternlab/source/js/components.js
@@ -58,62 +58,61 @@ $('.c-card--reveal').hover(function(){
  * Placeholder file for custom sub-theme behaviors.
  *
  */
-(function ($) {
-
-  /**
-  * Opens search filters modal.
-  */
-  // Open search filters.
-  $('#filter-button').click(function() {
-    $('#filters-modal').toggleClass('closed');
-  });
-  // Modal close button.
-  $('#filters-modal .close-button').click(function() {
-    $('#filters-modal').toggleClass('closed');
-  });
-
-  /**
-   * Adds counter and highlighting to Filter button.
-   */
-  $(function () {
-    $('#filter-button .counter').text(' ');
-    var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
-    if (len > 0) {
-      $("#filter-button .counter").text('(' + len + ')');
-    } else {
-      $("#filter-button .counter").text(' ');
-    }
-  })
-
-  function updateCounter() {
-    var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
-    if (len > 0) {
-      $("#filter-button .counter").text(' (' + len + ') ');
-    } else {
-      $("#filter-button .counter").text(' ');
-    }
-  }
-
-  $(".checkboxes-wrapper input:checkbox").on("change", function () {
-    updateCounter();
-    // Highlighting Filter button when there are filters selected.
-    var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
-    if ($(this).parents('.search-wrapper').find("#filter-button").hasClass('unfiltered') && len > 0) {
-      $("#filter-button").removeClass('unfiltered').addClass('filtered');
-    }
-    // Un-highlighting Filter button when there are no filters selected.
-    if (len == 0) {
-      $("#filter-button").addClass('unfiltered').removeClass('filtered');
-    }
-  });
-  // On load check if there are filters enabled and highlight Filter button.
-  var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
-  if (len > 0) {
-    $("#filter-button").removeClass('unfiltered').addClass('filtered');
-  }
-
-
-})(jQuery);
+// (function ($) {
+//   /**
+//   * Opens search filters modal.
+//   */
+//   // Open search filters.
+//   $('#filter-button').on('click', function() {
+//     $(this).closest('.search-wrapper').find('#filters-modal').toggleClass('closed');
+//   });
+//   // Modal close button.
+//   $('.close-button').on('click', function() {
+//     $(this).closest('#views-exposed-form-search-search').find('#filters-modal').toggleClass('closed');
+//   });
+//
+//   /**
+//    * Adds counter and highlighting to Filter button.
+//    */
+//   $(function () {
+//     $('#filter-button .counter').text(' ');
+//     var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
+//     if (len > 0) {
+//       $("#filter-button .counter").text('(' + len + ')');
+//     } else {
+//       $("#filter-button .counter").text(' ');
+//     }
+//   })
+//
+//   function updateCounter() {
+//     var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
+//     if (len > 0) {
+//       $("#filter-button .counter").text(' (' + len + ') ');
+//     } else {
+//       $("#filter-button .counter").text(' ');
+//     }
+//   }
+//
+//   $(".checkboxes-wrapper input:checkbox").on("change", function () {
+//     updateCounter();
+//     // Highlighting Filter button when there are filters selected.
+//     var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
+//     if ($(this).parents('.search-wrapper').find("#filter-button").hasClass('unfiltered') && len > 0) {
+//       $("#filter-button").removeClass('unfiltered').addClass('filtered');
+//     }
+//     // Un-highlighting Filter button when there are no filters selected.
+//     if (len == 0) {
+//       $("#filter-button").addClass('unfiltered').removeClass('filtered');
+//     }
+//   });
+//   // On load check if there are filters enabled and highlight Filter button.
+//   var len = $(".checkboxes-wrapper input[type='checkbox']:checked").length;
+//   if (len > 0) {
+//     $("#filter-button").removeClass('unfiltered').addClass('filtered');
+//   }
+//
+//
+// })(jQuery);
 
 
 
