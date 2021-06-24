a718567203250173f46ac6d713dd920cb5de1233
Savino <jason@kanopistudios.com>
2021-06-24 10:02:54 -0400

    rebuild dist files

--- dist/js/components.js
+++ dist/js/components.js
@@ -38,19 +38,19 @@
 
 })(jQuery);
 ;(function ($) {
-  
-  $(".media-object").click(function() {
-    window.location = $(this).find("h3 a").attr("href"); 
-    return false;
-  });
-
-})(jQuery);
-;(function ($) {
 
 // Open Card Reveal Click
 $('.c-card--reveal').hover(function(){
   $(this).find('.c-card__overlay').toggleClass('hover');
 });
+
+})(jQuery);
+;(function ($) {
+  
+  $(".media-object").click(function() {
+    window.location = $(this).find("h3 a").attr("href"); 
+    return false;
+  });
 
 })(jQuery);
 ;/**
